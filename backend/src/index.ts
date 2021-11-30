177cd4c09d9c44de4209290424ca38a7d286398b
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-30 22:39:16 +0000

    backend/watch added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,13 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.status(200).send({
+    body: 'Hello from Matchsic!',
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Matchsic backend is listening on port ${port}`);
 });
