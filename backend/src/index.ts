d8d46ba53746243efcdc464b66507c2a2d9cc3ac
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-30 22:47:54 +0000

    public folder added to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 
 
 const app = express();
+
+app.use(express.static('public'));
+
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/greetings', (req, res) => {
   res.status(200).send({
-    body: 'Hello from Matchsic!',
+    body: 'Hello from Matchsic!!',
   });
 });
 
