c399318f95019f16aedca6ceb0d8ca2c1439acef
EdgarNegrin <alu0101210964@ull.edu.es>
2021-12-03 19:38:53 +0000

    Background green icons

--- src/features/BottonBar.tsx
+++ src/features/BottonBar.tsx
@@ -1,23 +1,33 @@
 import React from "react"
 import { Container, Navbar } from "react-bootstrap"
+import classes from "./greenCircle.css"
 
 
 const barStyle = {
-  background: '#0BA55D',
-  position: 'absolute',
+  background: '#FFFF',
   bottom: 0,
   right: 0,
-  left: 0
+  left: 0,
+}
+
+
+const imageStyle = {
+  background: '#5cb85c',
+  borderRadius: 50,
+  flex: 1,
+  width: 50,
+  height: 50,
+  resizeMode: 'contain',
 }
 
 
 export const BottonBar = () => {
   return (
-    <Navbar style={barStyle}>
+    <Navbar className="fixed-bottom" style={barStyle}>
       <Container>
-        <Navbar.Brand href="http://localhost:3000/">Home</Navbar.Brand>
-        <Navbar.Brand href="http://localhost:3000/">Notificaciones</Navbar.Brand>
-        <Navbar.Brand href="http://localhost:3000/">Perfil</Navbar.Brand>
+        <Navbar.Brand href="http://localhost:3000/"><img style={imageStyle} src="../Home.svg"></img></Navbar.Brand>
+        <Navbar.Brand href="http://localhost:3000/"><img style={imageStyle} src="../notificacion.svg"></img></Navbar.Brand>
+        <Navbar.Brand href="http://localhost:3000/Profile"><img style={imageStyle} src="https://svgsilh.com/svg/1633250.svg"></img></Navbar.Brand>
       </Container>
     </Navbar>
   )
