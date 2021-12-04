daaebb4c268e7b7756432b034a4cbb30a82787ee
Miguel Martin <alu0101209777@ull.edu.es>
2021-12-04 03:17:29 +0000

    old code removed

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,14 +1,10 @@
 import React, { Fragment } from 'react'
-import { Outlet, Route, useNavigate } from 'react-router'
-import { Link, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router'
 import { BottomBar } from '../BottomBar';
 import { TopBar } from '../TopBar';
 
 export const HomePage = () => {
 
-  const tempTopBar = <div>top bar</div>;
-  const tempNavBar = <div>bottom bar</div>;
-  
   
   return (
     <Fragment>
