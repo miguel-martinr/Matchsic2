df65e504ecf2b52da9f7d9c9bfd773f7c281db8f
Miguel Martin <alu0101209777@ull.edu.es>
2021-12-04 03:31:14 +0000

    user is redirected from / to /home

--- src/features/home/MatchsicFrame.tsx
+++ src/features/home/MatchsicFrame.tsx
@@ -1,17 +1,25 @@
-import React, { Fragment } from 'react'
-import { Outlet } from 'react-router'
+import React, { Fragment, useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router'
 import { BottomBar } from '../BottomBar';
 import { TopBar } from '../TopBar';
 
 export const MatchsicFrame = () => {
+  const navigate = useNavigate();
 
-  
+  useEffect(() => {
+    const currentPath = window.location.href;
+
+    if (currentPath[currentPath.length - 1] === '/') { // If currentPath is root (/)
+      navigate('/home');  
+    }
+  }, []);
+
   return (
     <Fragment>
       {/*TopBar*/}
       <TopBar></TopBar>
 
-      
+
 
       <Outlet />
 
