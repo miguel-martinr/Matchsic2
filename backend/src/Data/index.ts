742c66b18786c358ebd757cac5bbc5245033da68
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-17 00:39:01 +0000

    dbService added

---
+++ backend/src/Data/index.ts
@@ -0,0 +1,13 @@
+import {UserInterface, UserModel} from './Models/user';
+
+
+const registerUser = async (user: UserInterface) => {
+  const newUser = new UserModel(user);
+  return newUser.save();
+};
+
+
+export const dbService = {
+  registerUser,
+};
+
