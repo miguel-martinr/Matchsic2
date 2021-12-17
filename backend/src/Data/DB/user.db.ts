e8954e1c2b5492a3fd60da819f369c74b26d06aa
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-17 01:07:13 +0000

    addUser name updated

--- backend/src/Data/DB/user.db.ts
+++ backend/src/Data/DB/user.db.ts
@@ -1,11 +1,11 @@
 import {UserInterface, UserModel} from '../Models/user';
 
-const registerUser = async (user: UserInterface) => {
+const addUser = async (user: UserInterface) => {
   const newUser = new UserModel(user);
   return newUser.save();
 };
 
 export const user = {
-  registerUser,
+  addUser,
 };
 
