6dd8ba879253f50e05081d69d2f32560e4206071
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-18 00:06:10 +0000

    postUser tests passing

--- backend/src/Data/DB/connect.db.ts
+++ backend/src/Data/DB/connect.db.ts
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config({path: '../.env'});
 
 
-const databaseURL = process.env.MONGODB_URL;
+const databaseURL = 'mongodb://localhost:27017/matchsic';
+//  databaseURL = process.env.MONGODB_URL;
 
 export const connectDB = async () => {
   try {
--- cypress/integration/backend/Routes/index.spec.ts
+++ cypress/integration/backend/Routes/index.spec.ts
@@ -1,5 +1,17 @@
-describe("dummy", () => {
-  it("Dummy test should be true", () => {
-    expect(true).to.equal(true);
+describe('API /user', () => {
+  it('It should be able to register a new user', () => {
+    const request = {
+      user: {
+        name: 'Vinicius Jr',
+        username: 'ViniJr',
+        email: 'vinijr@rmadrid.com',
+        password: '123456',
+      }
+    };
+
+    cy.request('POST', 'http://localhost:3000/user', request).then(res => {
+      expect(res.status).to.eq(201);
+      expect(res.body.message).to.eq('Welcome to Matchsic, ViniJr!');
+    })
   });
 });
