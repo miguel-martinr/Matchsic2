1fb4c98e1f1e2169f7bca3f1a04723984dd39340
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-18 01:54:11 +0000

    API login tests failing

--- cypress/integration/backend/Routes/index.spec.ts
+++ cypress/integration/backend/Routes/index.spec.ts
@@ -1,4 +1,13 @@
+import {config} from '../../../../backend/src/config';
+
+/** 
+ * Remember to change the mode to 'development' in the config.ts file
+ * before running these tests!
+ */
+
+
 describe('API /user', () => {
+
   it('It should be able to register a new user', () => {
     const request = {
       user: {
@@ -14,4 +23,20 @@ describe('API /user', () => {
       expect(res.body.message).to.eq('Welcome to Matchsic, ViniJr!');
     })
   });
+
+  it ('An existing user should be able to login', () => {
+    const request = {
+      user: {
+        username: 'ViniJr',
+        password: '123456',
+      }
+    };
+
+    cy.request('http://localhost:3000/user/login', request).then(res => {
+      expect(res.status).to.eq(200);
+      expect(res.headers['Set-Cookie']).to.exist;
+    })
+  });
 });
+
+
