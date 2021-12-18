335713482f8ad0810882e020b7526b906c73fe1b
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-18 02:09:30 +0000

    verify user in database added

---
+++ backend/src/Controllers/login.controller.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from 'express';
+import jwt from 'jsonwebtoken';
+
+export const getLogin = (req: Request, res: Response) => {
+  try {
+    const {username, password} = req.body;
+    
+  } catch (error) {
+
+  }
+};
