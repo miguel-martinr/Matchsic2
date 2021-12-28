15f92126e613047faddd893898d9827e635f02df
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-28 20:05:39 +0000

    marker added

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useAppSelector } from '../store/hooks';
 import classes from './HomePage.module.css';
 import { MatchsicTile } from './MatchsicTile';
+import { UserMarker } from './UserMarker';
 
 export const HomePage = () => {
+
+  const {userSession} = useAppSelector(state => state.matchsic);
   return (
     <Container fluid>
       <Row>
         <Col className={"text-center " + classes.gray_background}>
-          <MapContainer  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className={classes.map} >
+          <MapContainer  center={[28.47762873997604, -16.312475617040874]} zoom={13} scrollWheelZoom={true} className={classes.map} >
             
             <MatchsicTile />
-
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
+            
+            <UserMarker 
+              user={userSession} //temp
+            />
+            
           </MapContainer>
         </Col>
       </Row>
