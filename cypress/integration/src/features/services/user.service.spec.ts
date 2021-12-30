2301b4d3ccde2c81fff37c410ecf0ea49bd04506
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-30 16:48:58 +0000

    sucess case tested

--- cypress/integration/src/features/services/user.service.spec.ts
+++ cypress/integration/src/features/services/user.service.spec.ts
@@ -1,10 +1,11 @@
 describe('User service tests', () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  });
+
   it('Should redirect to /login if getData fails with 401', () => {
-    Cypress.on('uncaught:exception', (err, runnable) => {
-      // returning false here prevents Cypress from
-      // failing the test
-      return false
-    });
 
     cy.intercept('GET', '/user/data', {
       statusCode: 401,
@@ -19,4 +20,25 @@ describe('User service tests', () => {
     cy.wait('@getData').url().should('include', '/login');
 
   });
+
+  it('Should stay at /home if getData sucess (status 200)', () => {
+    cy.intercept('GET', '/user/data', {
+      statusCode: 200,
+      body: {
+        user: {
+          username: 'Lena_00',
+        }
+      }
+    }).as('getData');
+
+    cy.visit('/');
+    cy.get('input[id="username"]').type('Lena_00');
+    cy.get('input[id="password"]').type('1234');
+    cy.get('button').click();
+
+    cy.wait('@getData').url().should('include', '/home');
+
+  });
+
+
 });
