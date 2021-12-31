0680a7efe72c1643b83d5e3811f4959a5123917e
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-31 01:39:50 +0000

    activeUser schema fixed

--- backend/src/Data/Schemas/activeUser.ts
+++ backend/src/Data/Schemas/activeUser.ts
@@ -3,7 +3,6 @@ import * as mongoose from 'mongoose';
 export const ActiveUserSchema = new mongoose.Schema({
   userId: {type: String, required: true},
   location: {
-    required: true,
     coordinates: {
       latitude: {type: Number, required: true},
       longitude: {type: Number, required: true},
@@ -11,11 +10,9 @@ export const ActiveUserSchema = new mongoose.Schema({
   },
 
   music: {
-    required: true,
     trackName: {type: String, required: true},
     authorsNames: {type: [String], required: true},
     coverImage: {
-      required: true,
       url: {type: String, required: true},
       height: {type: Number, required: true},
       width: {type: Number, required: true},
@@ -24,9 +21,7 @@ export const ActiveUserSchema = new mongoose.Schema({
   },
 
   credentials: {
-    required: true,
     spotify: {
-      required: true, // ?
       accessToken: {type: String, required: true},
     },
   },
--- cypress/integration/backend/Routes/index.spec.ts
+++ cypress/integration/backend/Routes/index.spec.ts
@@ -86,7 +86,7 @@ describe('API /user', () => {
 
   it('It should retrieve near users as an array', () => {
     // With user logged in
-    cy.request('/near-users')
+    cy.request('/near-users', {id: '61bd4e30c59b1240d579b0b3'})
       .then(res => {
         expect(res.status).to.eq(200);
         expect(res.body.nearUsers).to.be.an('array');
