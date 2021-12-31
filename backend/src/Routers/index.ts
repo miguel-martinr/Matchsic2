60584c871bbe122748ed7971121452047e843fd4
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-31 02:42:17 +0000

    postActiveData route added

--- backend/src/Routers/index.ts
+++ backend/src/Routers/index.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import {getLogin, postUser} from '../Controllers';
+import { postActiveData } from '../Controllers/activeData.controller';
 import {getNearUsers} from '../Controllers/getNearUsers.controller';
 export const router = Router();
 
@@ -7,4 +8,5 @@ export const router = Router();
 router.post('/user', postUser);
 router.get('/login', getLogin);
 router.get('/near-users', getNearUsers);
+router.post('/user/active-data', postActiveData);
 
