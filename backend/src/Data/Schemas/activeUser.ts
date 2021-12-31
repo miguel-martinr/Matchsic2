89a096c4d2fac5e21139fda2864365023e12e690
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-31 03:21:29 +0000

    credentials removed from activeData schema

--- backend/src/Data/Schemas/activeUser.ts
+++ backend/src/Data/Schemas/activeUser.ts
@@ -20,9 +20,9 @@ export const ActiveUserSchema = new mongoose.Schema({
     status: {type: String, required: true},
   },
 
-  credentials: {
-    spotify: {
-      accessToken: {type: String, required: true},
-    },
-  },
+  // credentials: {
+  //   spotify: {
+  //     accessToken: {type: String, required: true},
+  //   },
+  // },
 });
--- cypress/integration/backend/Routes/index.spec.ts
+++ cypress/integration/backend/Routes/index.spec.ts
@@ -1,5 +1,5 @@
 import { config } from '../../../../backend/src/config';
-
+import { ActiveUserInterface } from '../../../../backend/src/Data/Models/activeUsers';
 /** 
  * Remember to change the mode to 'development' in the config.ts file
  * before running these tests!
@@ -84,14 +84,35 @@ describe('API /user', () => {
     });
   });
 
-  it('It should retrieve near users as an array', () => {
-    // With user logged in
-    cy.request('/near-users', {id: '61bd4e30c59b1240d579b0b3'})
-      .then(res => {
-        expect(res.status).to.eq(200);
-        expect(res.body.nearUsers).to.be.an('array');
-      });
-  });  
+  it("Should post a user's activeData", () => {
+    const activeData: ActiveUserInterface = {
+      userId: '61bd4e30c59b1240d579b0b3', // RodrygoG
+      location: {coordinates: {latitude: -23.5, longitude: -46.6}},
+      music: {
+        trackName: 'The Rover',
+        authorsNames: ['Led Zepellin'],
+        coverImage: {url: 'https://i.scdn.co/image/ab67616d0000b273f8f9b9b9b9b9b9b9b9b9b9b9b9', width: 300, height: 300},
+        status: 'playing',
+      },
+    };
+
+    cy.request('POST', '/user/active-data', {
+      activeData
+    }).then(res => {
+      expect(res.status).to.eq(200);
+      expect(res.body.message).to.eq('Active data has been set');
+    });
+  });
+
+
+  // it('It should retrieve near users as an array', () => {
+  //   // With user logged in
+  //   cy.request('/near-users', {id: '61bd4e30c59b1240d579b0b3'})
+  //     .then(res => {
+  //       expect(res.status).to.eq(200);
+  //       expect(res.body.nearUsers).to.be.an('array');
+  //     });
+  // });  
 });
 
 
