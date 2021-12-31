6d0fa1c005b6c9ec315fecbb01bb238b41650138
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-31 01:22:46 +0000

    getNearUsers route added

--- backend/src/Routers/index.ts
+++ backend/src/Routers/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import {getLogin, postUser} from '../Controllers';
+import {getNearUsers} from '../Controllers/getNearUsers.controller';
 export const router = Router();
 
 
 router.post('/user', postUser);
 router.get('/login', getLogin);
+router.get('/near-users', getNearUsers);
 
