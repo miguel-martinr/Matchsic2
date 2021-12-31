2ec5de037d09f768a3438e384912f94a1dcb72ae
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-31 01:21:11 +0000

    getNearUsers controller added

---
+++ backend/src/Controllers/getNearUsers.controller.ts
@@ -0,0 +1,24 @@
+import {Request, Response} from 'express';
+import {getNearUsersService} from '../Services';
+
+export const getLogin = async (req: Request, res: Response) => {
+  const error = {
+    statusCode: 500,
+    message: 'Unknown',
+  };
+
+  try {
+    const {id} = req.body;
+
+    if (!id) {
+      error.message = 'You must provide a user id';
+      error.statusCode = 400;
+      throw new Error();
+    }
+
+    const nearUsers = await getNearUsersService(id);
+    res.status(200).json({nearUsers});
+  } catch (error: any) {
+    res.status(error.statusCode).json({message: error.message});
+  }
+};
