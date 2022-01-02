83858acde65decbe30de36dafaef947882b07606
Carlos Delgado <carlsjdh@gmail.com>
2022-01-02 02:27:38 +0000

    test and auth function done

--- backend/src/Controllers/index.ts
+++ backend/src/Controllers/index.ts
@@ -1,2 +1,3 @@
 export {postUser} from './postUser.controller';
 export {getLogin} from './getLogin.controller';
+export {authorization} from './authorization';
--- cypress/integration/backend/Routes/index.spec.ts
+++ cypress/integration/backend/Routes/index.spec.ts
@@ -1,12 +1,56 @@
 import { config } from '../../../../backend/src/config';
+import {authorization} from '../../../../backend/src/Controllers'
+import {Request, Response, NextFunction} from 'express';
+import dotenv from 'dotenv';
+dotenv.config({path: '../.env'});
 
 /** 
  * Remember to change the mode to 'development' in the config.ts file
  * before running these tests!
  */
 
-
 describe('API /user', () => {
+  
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let nextFunction = () => {};
+
+  before(() => {
+    mockResponse = {
+        json: cy.spy().as('json'),
+        status: cy.spy().as('status'),
+    };
+
+  })
+
+
+  it('Authorization send empty token with no access_token', () => {
+    const expectedResponse = {
+      "message": "Empty Token"
+    };
+    mockRequest = {
+      cookies: {
+          access_token: ""
+      }
+  }
+      authorization(mockRequest as Request, mockResponse as Response, nextFunction);
+
+      expect(mockResponse.json).to.be.calledWith(Cypress.sinon.match(expectedResponse));
+  });
+
+  it('Authorization send Invalid token with wrong access_token', () => {
+    const expectedResponse = {
+      "message": "Invalid Token"
+    };
+    mockRequest = {
+      cookies: {
+          access_token: "1234"
+      }
+    }
+      authorization(mockRequest as Request, mockResponse as Response, nextFunction);
+
+      expect(mockResponse.json).to.be.calledWith(Cypress.sinon.match(expectedResponse));
+  });
 
   it('It should be able to register a new user', () => {
 
@@ -73,11 +117,11 @@ describe('API /user', () => {
     const request = {
       user: {
         username: 'RodrygoG',
-        password: '678910',
+        password: '123456',
       }
     };
 
-    cy.request('http://localhost:3000/login', request).then(res => {
+    return cy.request('http://localhost:3000/login', request).then(res => {
       expect(res.status).to.eq(200);
       expect(res.headers['set-cookie']).to.exist;
     });
