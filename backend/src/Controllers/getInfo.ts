8106b55f491a76be2d35981d1ae99cb0cb582e3d
Miguel Martín <alu0101209777@ull.edu.es>
2022-01-03 20:06:43 +0000

    getInfo status code updated

--- backend/src/Controllers/getInfo.ts
+++ backend/src/Controllers/getInfo.ts
@@ -8,7 +8,7 @@ export const getInfo = async (req: Request, res: Response) => {
   try {
     const {id} = req.body;
     // const existingUser = await verifyUserService(user);
-    return res.status(202).json({message: 'Your id is ' + id});
+    return res.status(200).json({message: 'Your id is ' + id});
   } catch (error: any) {
     return res.status(400).json({message: error.message});
   }
