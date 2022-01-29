2c8f6cd942c4d00309eb7bd2c03561c82e2e4dc5
Miguel Martín <alu0101209777@ull.edu.es>
2022-01-29 01:23:10 +0000

    locate logo src fix

--- src/features/home/MapControls/LocateControl.tsx
+++ src/features/home/MapControls/LocateControl.tsx
@@ -1,41 +1,8 @@
-import { createControlComponent } from "@react-leaflet/core";
 import L, { Control, DomUtil } from "leaflet";
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { TileLayer } from "react-leaflet";
 import { useAppSelector } from "../../store/hooks";
 import classes from './LocateControl.module.css';
-
-const MatchsicLocateControl = Control.extend({
-  onAdd: function (map: L.Map) {
-    const container = DomUtil.create('div');
-    container.style.backgroundColor = '#fff';
-    container.style.width = '3rem';
-    container.style.height = '3rem';
-    container.style.borderRadius = '50%';
-    container.style.display = 'flex';
-    container.style.marginBottom = '1rem';
-    container.onclick = () => {
-      console.log('Centering');
-      const userCurrentPosition = useAppSelector(state => state.matchsic.userSession.location.coordinates);
-      map.setView({ lat: userCurrentPosition.latitude, lng: userCurrentPosition.longitude }, 15);
-    };
-
-
-
-    const img = DomUtil.create('img');
-    img.src = "./assets/locate-logo.png";
-
-
-
-
-    container.appendChild(img);
-    return container;
-  },
-
-  onRemove: function (map: L.Map) { },
-
-});
+import locateLogo from "./locate-logo.png";
 
 interface LocateControlProps {
   map: L.Map | null,
@@ -55,7 +22,7 @@ export const LocateControl = (props: LocateControlProps) => {
       <div className={'leaflet-control ' + classes.controlContainer}>
         <img
           className={classes.controlLogo}
-          src='/assets/locate-logo.png' />
+          src={locateLogo} />
       </div>
     </div>
   )
