bf5cfe73c10de95dfbbd3b5523a3b52e44178574
Carlos Delgado <carlsjdh@gmail.com>
2022-02-01 00:10:02 +0000

    Fix cypress backend tests

--- cypress/integration/backend/Routes/01-User/03-getLogin.spec.ts
+++ cypress/integration/backend/Routes/01-User/03-getLogin.spec.ts
@@ -9,24 +9,37 @@ describe('Get stuff from users', () => {
       }
     };
 
-    return cy.request('/login', request).then(res => {
+    return cy.request('POST','/login', request).then(res => {
       expect(res.status).to.eq(200);
       expect(res.headers['set-cookie']).to.exist;
 
-      const existingUserCookie = res.headers['set-cookie'][0] as string;
+      // let existingUserCookie = res.headers['set-cookie'][0] as string;
+     
+      // expect(existingUserCookie).to.be.a('string');
       
-      expect(existingUserCookie).to.be.a('string');
+      cy.getCookies()
+      .should('have.length', 1)
+      .then((cookies) => {
+        expect(cookies[0]).to.have.property('name', 'access-token')
+        const payloadBuffer = Buffer.from(cookies[0].value.split('.')[1], 'base64');
+        const decodedPayloadJson = payloadBuffer.toString('ascii');
+        const payload = JSON.parse(decodedPayloadJson);
+
+        existingUserToken= cookies[0].value;
+  
+        expect(payload).to.have.property('id');
+        expect(payload.id).to.be.a('string');
+      })
+      // let cookie = existingUserCookie.split('=')[1];
+      // cookie = cookie.slice(0, cookie.indexOf(';'));
+      // existingUserToken = cookie; // To be used in next tests
       
-      let cookie = existingUserCookie.split('=')[1];
-      cookie = cookie.slice(0, cookie.indexOf(';'));
-      existingUserToken = cookie; // To be used in next tests
+      // const payloadBuffer = Buffer.from(cookie.split('.')[1], 'base64');
+      // const decodedPayloadJson = payloadBuffer.toString('ascii');
+      // const payload = JSON.parse(decodedPayloadJson);
 
-      const payloadBuffer = Buffer.from(cookie.split('.')[1], 'base64');
-      const decodedPayloadJson = payloadBuffer.toString('ascii');
-      const payload = JSON.parse(decodedPayloadJson);
-
-      expect(payload).to.have.property('id');
-      expect(payload.id).to.be.a('string');
+      // expect(payload).to.have.property('id');
+      // expect(payload.id).to.be.a('string');
 
     });
   });
