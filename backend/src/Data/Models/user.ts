03643005630d7efaa6cd838c63c97d0064b1d8bc
Miguel Martín <alu0101209777@ull.edu.es>
2022-02-04 05:52:18 +0000

    user model updated

--- backend/src/Data/Models/user.ts
+++ backend/src/Data/Models/user.ts
@@ -1,6 +1,11 @@
 import * as mongoose from 'mongoose';
 import {UserSchema} from '../Schemas/user';
 
+export interface SpotifyCredentials {
+  accessToken: string;
+  refreshToken: string;
+  expirationTime: number;
+}
 export interface UserInterface extends mongoose.Document {
   name: string,
   username: string,
@@ -9,6 +14,7 @@ export interface UserInterface extends mongoose.Document {
   description: string,
   musicPreference: [string],
   socialNetwork: [string],
+  spotifyCredentials: SpotifyCredentials,
 }
 
 export const UserModel = mongoose.model<UserInterface>('users', UserSchema);
