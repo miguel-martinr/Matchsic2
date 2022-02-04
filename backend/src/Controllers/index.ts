7a59d080ed62e1630da7c0b82cfd9a2f5f3a030a
Miguel Martín <alu0101209777@ull.edu.es>
2022-02-04 05:57:21 +0000

    imports fixed

--- backend/src/Controllers/index.ts
+++ backend/src/Controllers/index.ts
@@ -3,3 +3,5 @@ export {getLogin} from './getLogin.controller';
 export {getUserData} from './getUserData.controller';
 export {patchUserData} from './patchUserData.controller';
 export {spotifyConnect, spotifyCallback} from './spotify.controller';
+export {patchActiveData, postActiveData} from './activeData.controller';
+export {getNearUsers} from './getNearUsers.controller';
