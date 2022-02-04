08873e0fdb7c9ce993190e20c63059fb2cc922f3
Miguel Martín <alu0101209777@ull.edu.es>
2022-02-04 05:50:46 +0000

    index fix

--- backend/src/Controllers/index.ts
+++ backend/src/Controllers/index.ts
@@ -2,3 +2,4 @@ export {postUser} from './postUser.controller';
 export {getLogin} from './getLogin.controller';
 export {getUserData} from './getUserData.controller';
 export {patchUserData} from './patchUserData.controller';
+export {spotifyConnect, spotifyCallback} from './spotify.controller';
