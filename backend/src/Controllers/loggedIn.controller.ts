e7bd4983cb77110bc38d7b9da2c23e22046b01b4
Miguel Martín <alu0101209777@ull.edu.es>
2022-02-07 19:16:36 +0000

    amILoggedIn added

--- backend/src/Controllers/loggedIn.controller.ts
+++ backend/src/Controllers/loggedIn.controller.ts
@@ -1,13 +1,20 @@
 import {Request, Response} from 'express';
+import {getUserDataService} from '../Services';
 
 
 export const amILoggedIn = async (req: Request, res: Response) => {
   try {
+    const {id} = req.body;
+    if (id === undefined) throw new Error('Unknown user');
+    const userData = await getUserDataService(id);
     return res.status(200).json({
       amILoggedIn: true,
-      
-    })
+      userData,
+    });
   } catch (error: any) {
-    return res.status(400).json({message: error.message});
+    return res.status(403).json({
+      amILoggedIn: false,
+      reaosn: error.message || error,
+    });
   }
 };
