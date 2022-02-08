b37760f6728605758b0d6108d2c3afdc66ab9c3f
Miguel Martín <alu0101209777@ull.edu.es>
2022-02-08 19:57:40 +0000

    logout server side fix

--- backend/src/Controllers/logout.controller.ts
+++ backend/src/Controllers/logout.controller.ts
@@ -5,7 +5,11 @@ import {ActiveUserModel} from '../Data/Models/activeUsers';
 export const logout = async (req: Request, res: Response) => {
   try {
     const {id} = req.body;
-    res.clearCookie('access_token');
+    res.cookie('access-token', '', {
+      expires: new Date(0),
+      httpOnly: true,
+    });
+
     await ActiveUserModel.deleteOne({userId: id});
     return res.status(200).json({message: 'User has been logged out'});
   } catch (error: any) {
